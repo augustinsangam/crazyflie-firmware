21931da9e34d8721e284fdee17d10cb2f27ecdd7
Samba Bousso BAL <samba_bousso.bal@polymtl.ca>
2021-03-07 19:05:12 -0500

    Ajout main.c

--- src/main.c
+++ src/main.c
@@ -63,11 +63,11 @@ struct testPacketTX
   bool flying;
   bool ledOn;
 
-  uint16_t up;
-  uint16_t left;
-  uint16_t right;
   uint16_t front;
+  uint16_t left;
   uint16_t back;
+  uint16_t right;
+  uint16_t up;
 
 } __attribute__((packed));
 
