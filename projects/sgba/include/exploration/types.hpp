7e20d951f89d0da431fe7eee47a0ce690017b87f
Eya-Tom Augustin SANGAM <eya-tom-augustin.sangam@polymtl.ca>
2021-03-24 18:02:37 -0400

    Use C++ for types

--- projects/sgba/include/exploration/types.hpp
+++ projects/sgba/include/exploration/types.hpp
@@ -1,27 +1,28 @@
 #ifndef EXPLORATION_TYPES_HPP
 #define EXPLORATION_TYPES_HPP
 
+#include <cstddef>
 #include <cstdint>
 
 namespace exploration {
 
-/******************************************************************************/
+/******************************** radiolink.h *********************************/
 
-#define P2P_MAX_DATA_SIZE 60
+static constexpr std::size_t p2p_max_data_size{60};
 
-typedef struct _P2PPacket {
+using P2PPacket = struct P2PPacket {
 	uint8_t size; //< Size of data
 	uint8_t rssi; //< Received Signal Strength Intensity
 	union {
-		struct {
+		struct {          // NOLINT
 			uint8_t port; //< Header selecting channel and port
-			uint8_t data[P2P_MAX_DATA_SIZE]; //< Data
+			uint8_t data[p2p_max_data_size]; // NOLINT
 		};
-		uint8_t raw[P2P_MAX_DATA_SIZE + 1]; //< The full packet "raw"
+		uint8_t raw[p2p_max_data_size + 1]; // NOLINT
 	};
-} __attribute__((packed)) P2PPacket;
+} __attribute__((packed));
 
-/******************************************************************************/
+/***************************** stabilizer_types.h *****************************/
 
 struct vec3_s {
 	uint32_t timestamp; // Timestamp when the data was computed
@@ -31,41 +32,41 @@ struct vec3_s {
 	float z;
 };
 
-typedef struct vec3_s vector_t;
-typedef struct vec3_s point_t;
-typedef struct vec3_s velocity_t;
-typedef struct vec3_s acc_t;
+using vector_t = struct vec3_s;
+using point_t = struct vec3_s;
+using velocity_t = struct vec3_s;
+using acc_t = struct vec3_s;
 
-typedef struct attitude_s {
+using attitude_t = struct attitude_s {
 	uint32_t timestamp; // Timestamp when the data was computed
 
 	float roll;
 	float pitch;
 	float yaw;
-} attitude_t;
+};
 
-typedef struct quaternion_s {
+using quaternion_t = struct quaternion_s {
 	uint32_t timestamp;
 
 	union {
-		struct {
+		struct { // NOLINT
 			float q0;
 			float q1;
 			float q2;
 			float q3;
 		};
-		struct {
+		struct { // NOLINT
 			float x;
 			float y;
 			float z;
 			float w;
 		};
 	};
-} quaternion_t;
+};
 
-typedef enum mode_e { modeDisable = 0, modeAbs, modeVelocity } stab_mode_t;
+using stab_mode_t = enum mode_e { modeDisable = 0, modeAbs, modeVelocity };
 
-typedef struct setpoint_s {
+using setpoint_t = struct setpoint_s {
 	uint32_t timestamp;
 
 	attitude_t attitude;     // deg
@@ -87,7 +88,7 @@ typedef struct setpoint_s {
 		stab_mode_t yaw;
 		stab_mode_t quat;
 	} mode;
-} setpoint_t;
+};
 
 /******************************************************************************/
 
